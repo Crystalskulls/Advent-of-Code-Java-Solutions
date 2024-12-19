612637f23b2bd4bda447bb2a25f26a09d7e3cd8b
Benedikt Müller <bene93.mueller@web.de>
2024-12-19 14:10:04 +0100

    :construction: day 16 Part 1

---
+++ src/main/java/com/github/crystalskulls/aoc/_2024/day16/Day16.java
@@ -0,0 +1,160 @@
+package com.github.crystalskulls.aoc._2024.day16;
+
+import com.github.crystalskulls.aoc._2023.day11.Node;
+import com.github.crystalskulls.aoc._2024.day10.Day10;
+import com.github.crystalskulls.aoc.common.FileReader;
+import com.github.crystalskulls.aoc.common.Puzzle;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+import java.util.*;
+
+public class Day16 extends Puzzle {
+
+    Map<Vector2D, Tile> tiles = new HashMap<>();
+    Tile startTile;
+    Tile endTile;
+    int mazeWidth;
+    int mazeHeight;
+
+    public Day16() {
+        this.inputFile = "src/main/java/com/github/crystalskulls/aoc/_2024/day16/input.txt";
+    }
+
+    @Override
+    protected void solvePart1() {
+        List<Tile> queue = new ArrayList<>();
+        queue.add(startTile);
+        solveMaze(queue);
+        System.out.println("Part 1: " + endTile.score);
+        draw();
+    }
+
+    @Override
+    protected void solvePart2() {
+
+    }
+
+    @Override
+    protected void readInputData(String inputFile) {
+        List<String> lines = FileReader.readAllLines(inputFile);
+        mazeHeight = lines.size();
+        mazeWidth = lines.getFirst().length();
+        for (int y = 0; y < lines.size(); y++) {
+            char[] chars = lines.get(y).toCharArray();
+            for (int x = 0; x < chars.length; x++) {
+                Vector2D currentVector = new Vector2D(x, y);
+                Tile tile = new Tile(currentVector, Type.fromString(chars[x] + ""));
+                if(tile.type.equals(Type.START)) {
+                    tile.score = 0;
+                    startTile = tile;
+                } else if (tile.type.equals(Type.END)) {
+                    endTile = tile;
+                }
+                tiles.put(currentVector, tile);
+            }
+        }
+    }
+
+    private void solveMaze(List<Tile> queue) {
+        while(!queue.isEmpty()) {
+            Tile tile = queue.removeFirst();
+            List<Tile> nextTiles = getNextFreeTiles(tile);
+            for (Tile nextFreeTile : nextTiles) {
+                queue.add(nextFreeTile);
+                int tmpScore = tile.score + 1;
+                if(nextFreeTile.position.getX() == tile.position.getX() && nextFreeTile.position.getY() < tile.position.getY()) {
+                    nextFreeTile.direction = Direction.NORTH;
+                } else if (nextFreeTile.position.getX() > tile.position.getX() && nextFreeTile.position.getY() == tile.position.getY()) {
+                    nextFreeTile.direction = Direction.EAST;
+                } else if (nextFreeTile.position.getX() == tile.position.getX() && nextFreeTile.position.getY() > tile.position.getY()) {
+                    nextFreeTile.direction = Direction.SOUTH;
+                } else {
+                    nextFreeTile.direction = Direction.WEST;
+                }
+                if(nextFreeTile.direction != tile.direction) {
+                    tmpScore += 1000;
+                }
+                if(nextFreeTile.score == null || tmpScore <= nextFreeTile.score) {
+                    nextFreeTile.score = tmpScore;
+                }
+                nextFreeTile.visitedBy.add(tile);
+            }
+            queue.sort(Comparator.comparingInt(t -> t.score));
+        }
+    }
+
+    private void draw() {
+        for (int y = 0; y < mazeHeight; y++) {
+            for (int x = 0; x < mazeWidth; x++) {
+                Tile tile = tiles.get(new Vector2D(x, y));
+                if(tile.type.equals(Type.FREE)) {
+                    System.out.print(tile.direction.c);
+                } else {
+                    System.out.print(tiles.get(new Vector2D(x, y)).type.c);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static class Tile {
+        private Vector2D position;
+        private Type type;
+        Integer score;
+        Direction direction;
+        List<Tile> visitedBy = new ArrayList<>();
+
+        Tile(Vector2D position, Type type) {
+            this.type = type;
+            this.position = position;
+            this.direction = Direction.NONE;
+        }
+
+    }
+
+    List<Tile> getNextFreeTiles(Tile tile) {
+        List<Tile> nextFreeTiles = new ArrayList<>();
+        nextFreeTiles.add(tiles.get(tile.position.add(new Vector2D(-1, 0))));
+        nextFreeTiles.add(tiles.get(tile.position.add(new Vector2D(1, 0))));
+        nextFreeTiles.add(tiles.get(tile.position.add(new Vector2D(0, -1))));
+        nextFreeTiles.add(tiles.get(tile.position.add(new Vector2D(0, 1))));
+        return nextFreeTiles.stream().filter(t -> t.type.equals(Type.FREE) || t.type.equals(Type.END)).filter(t -> !tile.visitedBy.contains(t)).toList();
+    }
+
+    private enum Direction {
+        NORTH("^"), EAST(">"), SOUTH("v"), WEST("<"), NONE(".");
+
+        String c;
+        Direction(String c) {
+            this.c = c;
+        }
+
+        public static Type fromString(String text) {
+            for (Type type : Type.values()) {
+                if (type.c.equalsIgnoreCase(text)) {
+                    return type;
+                }
+            }
+            return null;
+        }
+    }
+
+    private enum Type {
+        WALL("#"), START("S"), END("E"), FREE(".");
+
+        String c;
+        Type(String c) {
+            this.c = c;
+        }
+
+        public static Type fromString(String text) {
+            for (Type type : Type.values()) {
+                if (type.c.equalsIgnoreCase(text)) {
+                    return type;
+                }
+            }
+            return null;
+        }
+    }
+}
